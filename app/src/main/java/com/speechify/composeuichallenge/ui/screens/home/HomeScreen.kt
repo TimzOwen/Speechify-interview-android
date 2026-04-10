b5089e527fcf87cab94fb8f90a668d208eaffaf4
timzowen <timzowen@gmail.com>
2026-04-10 15:06:36 +0300

    Scaffoled for home screen

--- app/src/main/java/com/speechify/composeuichallenge/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/speechify/composeuichallenge/ui/screens/home/HomeScreen.kt
@@ -14,8 +14,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -27,7 +30,7 @@ import com.speechify.composeuichallenge.data.Book
 import com.speechify.composeuichallenge.ui.screens.home.components.BooksCardComponent
 import com.speechify.composeuichallenge.ui.screens.home.components.SearchComponent
 
-@OptIn(ExperimentalSharedTransitionApi::class)
+@OptIn(ExperimentalSharedTransitionApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     sharedTransitionScope: SharedTransitionScope,
@@ -37,31 +40,39 @@ fun HomeScreen(
 ) {
     val uiState by viewModel.uiState.collectAsState()
 
-    when (val state = uiState) {
-        is HomeUiState.Loading -> LoadingUIState()
-        is HomeUiState.Error -> ErrorState(state.message)
-        is HomeUiState.Success -> {
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxSize()
-            ) {
-                SearchComponent(
-                    query = state.searchQuery,
-                    onQueryChanged = viewModel::onSearchQueryChanged
-                )
-                Spacer(modifier = Modifier.height(12.dp))
-                AnimatedContent(
-                    targetState = state.books,
-                    label = "books_filter_animation"
-                ) { books ->
-                    LoadBooks(
-                        books = books,
-                        sharedTransitionScope = sharedTransitionScope,
-                        animatedVisibilityScope = animatedVisibilityScope,
-                        onNavigate = onNavigate,
-                        modifier = Modifier.weight(1f)
+    Scaffold(
+        topBar = { TopAppBar(
+            {Text("Books App")}
+        ) }
+    ) { paddingValues ->
+
+        when (val state = uiState) {
+            is HomeUiState.Loading -> LoadingUIState()
+            is HomeUiState.Error -> ErrorState(state.message)
+            is HomeUiState.Success -> {
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .padding(paddingValues)
+                        .fillMaxSize()
+                ) {
+                    SearchComponent(
+                        query = state.searchQuery,
+                        onQueryChanged = viewModel::onSearchQueryChanged
                     )
+                    Spacer(modifier = Modifier.height(12.dp))
+                    AnimatedContent(
+                        targetState = state.books,
+                        label = "books_filter_animation"
+                    ) { books ->
+                        LoadBooks(
+                            books = books,
+                            sharedTransitionScope = sharedTransitionScope,
+                            animatedVisibilityScope = animatedVisibilityScope,
+                            onNavigate = onNavigate,
+                            modifier = Modifier.weight(1f)
+                        )
+                    }
                 }
             }
         }
